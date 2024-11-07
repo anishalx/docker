0f1e6ccb2cf63551ea2c47487b0c584fa9283a16
Anish <radlysamar@gmail.com>
2024-11-07 11:32:25 +0530

    just checking modified or not

--- node/frontend/src/App.js
+++ node/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>HI there</code> and save to reload.
+           HELLO me ok now working perfectly
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn me 
         </a>
       </header>
     </div>
